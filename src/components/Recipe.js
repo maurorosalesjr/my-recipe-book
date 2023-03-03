66ab4540933d3bc084fb360b10fb132ee192bb62
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2023-03-03 10:06:26 -0800

    add recipe and recipelist js files and configuration of proptypes

---
+++ src/components/Recipe.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Recipe(props){
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
+
+Recipe.propTypes = {
+  name: PropTypes.string.isRequired,
+  ingredients: PropTypes.string.isRequired,
+  instructions: PropTypes.string.isRequired,
+  photo: PropTypes.string,
+  id: PropTypes.string,
+}
