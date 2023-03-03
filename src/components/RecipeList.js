79b71c7de123e637e0b190e9459c02a38ad2a554
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2023-03-03 10:36:43 -0800

    flesh out recipelist

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -6,14 +6,25 @@ import Recipe from "./Recipe";
 function RecipeList(props){
   return (
     <React.Fragment>
-
+      <hr/>
+        {
+          props.recipeList.map((recipe) =>
+          <Recipe 
+            whenRecipeClicked={props.onRecipeSelection} 
+            name={recipe.name}
+            ingredients={recipe.ingredients}
+            instructions={recipe.instructions}
+            photo={recipe.photo}
+            id={recipe.id}
+            key={recipe.key}/>
+          )}
     </React.Fragment>
   );
 }
 
 RecipeList.propTypes = {
   recipeList: PropTypes.array,
-
+  onRecipeSelection: PropTypes.func
 }
 
 export default RecipeList;
